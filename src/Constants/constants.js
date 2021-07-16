8afef59277761601e4880179ba28b5a78e76dffd
Praveen S Nair <praveen_772@yahoo.co.in>
2021-07-16 11:06:31 +0530

    Updates

--- src/Constants/constants.js
+++
@@ -1,3 +0,0 @@
-export const baseUrl = "https://api.themoviedb.org/3";
-export const API_KEY = "518c176c61bb1068d5c23b86c6d84033";
-export const imageUrl = "https://image.tmdb.org/t/p/original";
--- src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-import {baseUrl} from "./Constants/constants"
-
-const instance = axios.create({
-    baseURL: baseUrl
-    
-  });
-
-  export default instance
